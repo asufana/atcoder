62361e978f0b4b50f900885cf6bfd901d07ff794
asufana <asufanah@gmail.com>
2023-06-07 22:31:07 +0900

    wip

--- abc292/b/Main.hs
+++ abc292/b/Main.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE TupleSections #-}
-
-import Control.Monad (replicateM)
 import qualified Data.IntMap.Strict as IM
+import Control.Monad.List (replicateM, replicateM_)
 import Data.Maybe (fromMaybe)
 main :: IO ()
 main = do
@@ -9,14 +8,14 @@ main = do
   [n,q] <- map read . words <$> getLine :: IO [Int]
   qa <- replicateM q $ map read . words <$> getLine :: IO [[Int]]
 
-  let acc = IM.fromList $ map (,0) [1..n]
-  let (acc', results) = foldl calc (acc, []) qa
-  let results' = map (\x -> if x then "Yes" else "No") $ reverse results
-  mapM_ putStrLn results'
-  
-calc (acc, results) [e, x]
-  | e == 1 = (IM.adjust (+1) x acc, results)
-  | e == 2 = (IM.adjust (+2) x acc, results)
-  | otherwise = if f then (acc, True:results) else (acc, False:results)
-  where f = fromMaybe 0 (IM.lookup x acc) > 1
+  let acc = IM.fromAscList $ map (,0) [1..n]
+  let (ret,_) = foldl fSolve ([],acc) qa
+  mapM_ putStrLn $ map (\x -> if x then "Yes" else "No")  ret
+
+fSolve (ret,acc) [e,x]
+  | e == 1 = (ret,IM.adjust (+1) x acc)
+  | e == 2 = (ret,IM.adjust (+2) x acc)
+  | otherwise = if f then (ret++[True],acc) else (ret++[False],acc)
+      where f = fromMaybe 0 (IM.lookup x acc) > 1
+
 
