8597da78d765eed7c45b5d518b2f87cd6c324910
asufana <asufanah@gmail.com>
2023-06-08 22:22:47 +0900

    wip

--- abc284/b/Main.hs
+++ abc284/b/Main.hs
@@ -5,6 +5,5 @@ main = do
   t <- readLn :: IO Int
   as <- replicateM t (replicateM 2 $ map read . words <$> getLine :: IO [[Int]])
 
-  let as' = map (\[_,ts] -> length $ filter id $ map odd ts) as
-  mapM_ print as'
+  mapM_ print $ map (\[_,ts] -> length $ filter id $ map odd ts) as
 
