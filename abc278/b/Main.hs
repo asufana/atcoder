72926ceb4261be5c0638c6f2b7e703413c1d2d09
asufana <asufanah@gmail.com>
2023-06-21 20:44:39 +0900

    wip

--- abc278/b/Main.hs
+++ abc278/b/Main.hs
@@ -3,10 +3,10 @@ main = do
 
   [h,m] <- map read . words <$> getLine :: IO [Int]
 
-  let (h',m') = until fSolve fNext (h,m)
-  putStrLn $ unwords [show h',show m']
+  let (h',m') = until fEval fNext (h,m)
+  putStrLn $ unwords [show h', show m']
 
-fSolve (h,m) = b<=3 && c<=3
+fEval (h,m) = b <= 5 && (a < 2 || c <= 3)
   where
     (a,b) = divMod h 10
     (c,d) = divMod m 10
