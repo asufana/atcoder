ada6d7cb15f1466a3fda512eb3ed729d092f26f1
asufana <asufanah@gmail.com>
2023-07-16 19:57:10 +0900

    wip

--- abc307/a/Main.hs
+++ abc307/a/Main.hs
@@ -2,11 +2,9 @@ main :: IO ()
 main = do
 
   n <- readLn :: IO Int
-  as <- map read . words <$> getLine :: IO [Int ]
-
+  as <- map read . words <$> getLine :: IO [Int]
   putStrLn $ unwords $ map show $ fSolve as
 
-fSolve :: [Int] -> [Int]
 fSolve [] = []
-fSolve (a1:a2:a3:a4:a5:a6:a7:as) = (a1+a2+a3+a4+a5+a6+a7) : fSolve as
-
+fSolve as = sum as1 : fSolve ass
+  where (as1,ass) = splitAt 7 as
