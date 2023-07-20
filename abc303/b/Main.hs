c019ea71800f1daadba5d6408e9f32d9762c13d4
asufana <asufanah@gmail.com>
2023-07-20 23:04:50 +0900

    wip

--- abc303/b/Main.hs
+++ abc303/b/Main.hs
@@ -1,22 +1,21 @@
-import Control.Monad (replicateM)
-import Data.List (sort)
+import Control.Monad.List (replicateM)
+import Data.List (sort, nub)
 main :: IO ()
 main = do
 
   [n,m] <- map read . words <$> getLine :: IO [Int]
-  as <- replicateM m $ map read . words <$> getLine :: IO [[Int]]
+  ass <- replicateM m $ map read . words <$> getLine :: IO [[Int]]
 
-  let as' = concatMap pair as
-  let acc = [(x,y) | x<-[1..n],y<-[1..n],x/=y,x<y]
-  let as'' = foldl exists acc as'
-  print $ length as''
+  let whl = [(x,y)|x<-[1..n],y<-[1..n],x<y]
+  let ass' = nub $ sort $ concatMap fPair ass
+  print $ length $ filter not $ map (`elem` ass') whl
+
+fPair [] = []
+fPair [a1,a2]
+  | a1 < a2 = [(a1,a2)]
+  | otherwise = [(a2,a1)]
+fPair (a1:a2:as)
+  | a1 < a2 = (a1,a2) : fPair (a2:as)
+  | otherwise = (a2,a1) : fPair (a2:as)
 
 
-pair [] = []
-pair [a1,a2] = [sortpair (a1,a2)]
-pair (a1:a2:as) = sortpair (a1,a2) : pair (a2:as)
-sortpair (a1,a2) = if a1 < a2 then (a1,a2) else (a2,a1)
-
-exists acc (a1,a2)
-  | (a1,a2) `elem` acc = filter (\(x1,x2) -> (a1,a2)/=(x1,x2)) acc
-  | otherwise = acc
