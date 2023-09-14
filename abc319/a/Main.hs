f99d23eb31ca434989bf00fd0eac4209c09b130e
asufana <asufanah@gmail.com>
2023-09-14 22:02:45 +0900

    wip

---
+++ abc319/a/Main.hs
@@ -0,0 +1,10 @@
+import qualified Data.Map.Strict as DM
+
+main :: IO ()
+main = do
+
+  s <- getLine
+
+  let lst = DM.fromList [("tourist",3858), ("ksun48",3679), ("Benq",3658), ("Um_nik",3648), ("apiad",3638), ("Stonefeang",3630), ("ecnerwala",3613), ("mnbvmar",3555), ("newbiedmy",3516), ("semiexp",3481)]
+  print $ DM.findWithDefault 0 s lst
+
