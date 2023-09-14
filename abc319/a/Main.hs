38f61b560e22424659be646ab988385706925c6e
asufana <asufanah@gmail.com>
2023-09-14 22:15:52 +0900

    wip

--- abc319/a/Main.hs
+++ abc319/a/Main.hs
@@ -1,10 +1,21 @@
 import qualified Data.Map.Strict as DM
+import Data.List.Extra (list)
 
 main :: IO ()
 main = do
 
   s <- getLine
 
-  let lst = DM.fromList [("tourist",3858), ("ksun48",3679), ("Benq",3658), ("Um_nik",3648), ("apiad",3638), ("Stonefeang",3630), ("ecnerwala",3613), ("mnbvmar",3555), ("newbiedmy",3516), ("semiexp",3481)]
-  print $ DM.findWithDefault 0 s lst
+  let list = [("tourist",3858), ("ksun48",3679), ("Benq",3658), ("Um_nik",3648), ("apiad",3638), ("Stonefeang",3630), ("ecnerwala",3613), ("mnbvmar",3555), ("newbiedmy",3516), ("semiexp",3481)]
+
+  -- 内包表記を使う
+  print $ [v|(k,v)<-list,k==s]!!0
+
+  -- Data.Mapを使う
+  -- let list' = DM.fromList list
+  -- print $ DM.findWithDefault 0 s list'
+
+  -- lookupを使う
+  -- let Just v = lookup s list
+  -- print v
 
